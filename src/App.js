59b808be19e08cb4bb0e0d2965f33c4e2ffa81a5
Lohit P Talavar <128409310+lohitpt252003@users.noreply.github.com>
2025-01-10 10:44:28 +0000

    INIT

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import CodeEditor from './components/CodeEditor/CodeEditor';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CodeEditor />
+    </div>
+  );
+};
+
+export default App;
