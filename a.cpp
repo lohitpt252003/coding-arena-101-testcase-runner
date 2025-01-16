f09a660209bc77db141471682905d98ccd7375ff
lohitpt252003 <lohitpt252003@gmail.com>
2025-01-16 15:36:52 +0530

    Dump

---
+++ a.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ws ' '
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define YES cout << "YES"
+#define NO cout << "NO"
+#define yes cout << "yes"
+#define no cout << "no"
+#define _sz(x) (int) x.size()
+
+const int INF = (int) 1e9;
+const ll ll_INF = (ll) 1e18;
+const ll MOD = 1e9 + 7;
+
+inline void scan(auto &arr, const auto &size_of_arr) {
+    for (int i = 0; i < size_of_arr; i++) cin >> arr[i];
+}
+
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(__null);
+    
+    int t = 1;
+    cin >> t;
+
+    for (int _ = 0; _ < t; _++) solve();
+
+    return 0;
+}
